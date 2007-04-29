36b8285725bef8324164dd530c3a4a1a2ce7d052
mathieuk <mathieuk@8941dcae-ad2d-0410-8676-e30e110ce8a3>
2007-04-29 13:39:20 +0000

    - Remove all references to FooBar in the LGPL header and replace them with XdebugClient. Oops :P
    git-svn-id: http://xdebugclient.googlecode.com/svn/trunk@10 8941dcae-ad2d-0410-8676-e30e110ce8a3

--- xdc/Forms/AboutForm.cs
+++ xdc/Forms/AboutForm.cs
@@ -9,7 +9,7 @@
  *  the Free Software Foundation; either version 2.1 of the License, or
  *  (at your option) any later version.
 
- *  Foobar is distributed in the hope that it will be useful,
+ *  XDebugClient is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
--- xdc/MainForm.cs
+++ xdc/MainForm.cs
@@ -9,13 +9,13 @@
  *  the Free Software Foundation; either version 2.1 of the License, or
  *  (at your option) any later version.
 
- *  Foobar is distributed in the hope that it will be useful,
+ *  XDebugClient is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
  *
  *  You should have received a copy of the GNU Lesser General Public License
- *  along with Foobar; if not, write to the Free Software
+ *  along with XDebugClient; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
